3bf9a2234a0b5b6c6c4e3fa73f08ef521e352e1a
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-13 04:31:06 +0000

    sc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 # Streamlitのページ設定
-st.set_page_config(page_title="3D RGB Visualizer", layout="wide")
+st.set_page_config(page_title="3D RGB Cube Visualizer")
 
 # ヘッダー
-st.title("3D RGB Visualizer")
+st.title("3D RGB Cube Visualizer")
 st.caption("Created by Daiki Ito")
-st.subheader("３次元グラフを描画することができます")
+st.subheader("RGB値から半透明の立方体を描画します")
 
 # RGB値をユーザーから取得
 r1 = st.number_input('R1:', min_value=0, max_value=255, value=200)
@@ -19,16 +19,46 @@ r2 = st.number_input('R2:', min_value=0, max_value=255, value=250)
 g2 = st.number_input('G2:', min_value=0, max_value=255, value=200)
 b2 = st.number_input('B2:', min_value=0, max_value=255, value=100)
 
-# DataFrameの作成
-df = pd.DataFrame({
-    'x': [r1, r2],
-    'y': [g1, g2],
-    'z': [b1, b2],
-    'color': ['Point 1', 'Point 2']
-})
+# 立方体のサイズ
+cube_size = 10
 
 # 3Dプロットの作成
-fig = px.scatter_3d(df, x='x', y='y', z='z', color='color', color_discrete_map={'Point 1':'rgb(255,0,0)', 'Point 2':'rgb(0,255,0)'})
+fig = go.Figure()
+
+# 立方体1
+fig.add_trace(go.Mesh3d(
+    x=[r1 - cube_size / 2, r1 + cube_size / 2, r1 + cube_size / 2, r1 - cube_size / 2, r1 - cube_size / 2,
+       r1 - cube_size / 2, r1 + cube_size / 2, r1 + cube_size / 2],
+    y=[g1 - cube_size / 2, g1 - cube_size / 2, g1 + cube_size / 2, g1 + cube_size / 2, g1 - cube_size / 2,
+       g1 - cube_size / 2, g1 - cube_size / 2, g1 + cube_size / 2],
+    z=[b1 - cube_size / 2, b1 - cube_size / 2, b1 - cube_size / 2, b1 - cube_size / 2, b1 - cube_size / 2,
+       b1 + cube_size / 2, b1 + cube_size / 2, b1 + cube_size / 2],
+    opacity=0.5,
+    color='red',
+    showscale=False
+))
+
+# 立方体2
+fig.add_trace(go.Mesh3d(
+    x=[r2 - cube_size / 2, r2 + cube_size / 2, r2 + cube_size / 2, r2 - cube_size / 2, r2 - cube_size / 2,
+       r2 - cube_size / 2, r2 + cube_size / 2, r2 + cube_size / 2],
+    y=[g2 - cube_size / 2, g2 - cube_size / 2, g2 + cube_size / 2, g2 + cube_size / 2, g2 - cube_size / 2,
+       g2 - cube_size / 2, g2 - cube_size / 2, g2 + cube_size / 2],
+    z=[b2 - cube_size / 2, b2 - cube_size / 2, b2 - cube_size / 2, b2 - cube_size / 2, b2 - cube_size / 2,
+       b2 + cube_size / 2, b2 + cube_size / 2, b2 + cube_size / 2],
+    opacity=0.5,
+    color='green',
+    showscale=False
+))
+
+# プロットの設定
+fig.update_layout(
+    scene=dict(
+        xaxis=dict(title='R'),
+        yaxis=dict(title='G'),
+        zaxis=dict(title='B'),
+    )
+)
 
 # プロットの表示
 st.plotly_chart(fig)
