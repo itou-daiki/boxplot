aa31d0c4bf6f006f5dc3d81289d639fe37a3d54b
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-13 04:39:53 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,16 @@ st.caption("Created by Daiki Ito")
 st.subheader("RGB値から半透明の立方体を描画します")
 
 # RGB値をユーザーから取得
-r1 = st.number_input('R1:', min_value=0, max_value=255, value=0)
-g1 = st.number_input('G1:', min_value=0, max_value=255, value=0)
-b1 = st.number_input('B1:', min_value=0, max_value=255, value=0)
-
-r2 = st.number_input('R2:', min_value=0, max_value=255, value=0)
-g2 = st.number_input('G2:', min_value=0, max_value=255, value=0)
-b2 = st.number_input('B2:', min_value=0, max_value=255, value=0)
+col1, col2, col3 = st.columns(3)
+with col1:
+    r1 = st.number_input('R1:', min_value=0, max_value=255, value=0)
+    r2 = st.number_input('R2:', min_value=0, max_value=255, value=0)
+with col2:
+    g1 = st.number_input('G1:', min_value=0, max_value=255, value=0)
+    g2 = st.number_input('G2:', min_value=0, max_value=255, value=0)
+with col3:
+    b1 = st.number_input('B1:', min_value=0, max_value=255, value=0)
+    b2 = st.number_input('B2:', min_value=0, max_value=255, value=0)
 
 # 立方体のサイズ
 cube_size = 10
@@ -27,12 +30,9 @@ fig = go.Figure()
 
 # 立方体1
 fig.add_trace(go.Mesh3d(
-    x=[r1 - cube_size / 2, r1 + cube_size / 2, r1 + cube_size / 2, r1 - cube_size / 2, r1 - cube_size / 2,
-       r1 - cube_size / 2, r1 + cube_size / 2, r1 + cube_size / 2],
-    y=[g1 - cube_size / 2, g1 - cube_size / 2, g1 + cube_size / 2, g1 + cube_size / 2, g1 - cube_size / 2,
-       g1 - cube_size / 2, g1 - cube_size / 2, g1 + cube_size / 2],
-    z=[b1 - cube_size / 2, b1 - cube_size / 2, b1 - cube_size / 2, b1 - cube_size / 2, b1 - cube_size / 2,
-       b1 + cube_size / 2, b1 + cube_size / 2, b1 + cube_size / 2],
+    x=[r1, r1 + cube_size, r1 + cube_size, r1, r1, r1, r1 + cube_size, r1 + cube_size],
+    y=[g1, g1, g1 + cube_size, g1 + cube_size, g1, g1, g1, g1 + cube_size],
+    z=[b1, b1, b1, b1, b1 + cube_size, b1 + cube_size, b1 + cube_size, b1 + cube_size],
     opacity=0.5,
     color='red',
     showscale=False
@@ -40,12 +40,9 @@ fig.add_trace(go.Mesh3d(
 
 # 立方体2
 fig.add_trace(go.Mesh3d(
-    x=[r2 - cube_size / 2, r2 + cube_size / 2, r2 + cube_size / 2, r2 - cube_size / 2, r2 - cube_size / 2,
-       r2 - cube_size / 2, r2 + cube_size / 2, r2 + cube_size / 2],
-    y=[g2 - cube_size / 2, g2 - cube_size / 2, g2 + cube_size / 2, g2 + cube_size / 2, g2 - cube_size / 2,
-       g2 - cube_size / 2, g2 - cube_size / 2, g2 + cube_size / 2],
-    z=[b2 - cube_size / 2, b2 - cube_size / 2, b2 - cube_size / 2, b2 - cube_size / 2, b2 - cube_size / 2,
-       b2 + cube_size / 2, b2 + cube_size / 2, b2 + cube_size / 2],
+    x=[r2, r2 + cube_size, r2 + cube_size, r2, r2, r2, r2 + cube_size, r2 + cube_size],
+    y=[g2, g2, g2 + cube_size, g2 + cube_size, g2, g2, g2, g2 + cube_size],
+    z=[b2, b2, b2, b2, b2 + cube_size, b2 + cube_size, b2 + cube_size, b2 + cube_size],
     opacity=0.5,
     color='green',
     showscale=False
