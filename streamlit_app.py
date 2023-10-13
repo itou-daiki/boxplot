360cefe018bf47d97db7bb7a03e124d237cc3213
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-13 04:55:30 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,27 +31,14 @@ with col3:
 # 立方体のサイズ
 cube_size = st.slider("Cube Size:", min_value=1, max_value=100, value=10)
 
-# 頂点のインデックスを定義（立方体を構成する各三角形の3つの頂点）
-faces_idx = [
-    [0, 1, 2], [0, 2, 3],  # bottom
-    [4, 5, 6], [4, 6, 7],  # top
-    [0, 1, 5], [0, 5, 4],  # front
-    [2, 3, 7], [2, 7, 6],  # back
-    [0, 3, 7], [0, 7, 4],  # left
-    [1, 2, 6], [1, 6, 5],  # right
-]
-
 # 3Dプロットの作成
 fig = go.Figure()
 
 # 立方体1
 fig.add_trace(go.Mesh3d(
-    x=[r1, r1 + cube_size, r1 + cube_size, r1, r1, r1 + cube_size, r1 + cube_size, r1],
-    y=[g1, g1, g1 + cube_size, g1 + cube_size, g1, g1, g1 + cube_size, g1 + cube_size],
-    z=[b1, b1, b1, b1, b1 + cube_size, b1 + cube_size, b1 + cube_size, b1 + cube_size],
-    i=[idx[0] for idx in faces_idx],
-    j=[idx[1] for idx in faces_idx],
-    k=[idx[2] for idx in faces_idx],
+    x=[0, r1, r1, 0, 0, r1, r1, 0],
+    y=[0, 0, g1, g1, 0, 0, g1, g1],
+    z=[0, 0, 0, 0, b1, b1, b1, b1],
     opacity=0.5,
     color='red',
     showscale=False
@@ -59,12 +46,9 @@ fig.add_trace(go.Mesh3d(
 
 # 立方体2
 fig.add_trace(go.Mesh3d(
-    x=[r2, r2 + cube_size, r2 + cube_size, r2, r2, r2 + cube_size, r2 + cube_size, r2],
-    y=[g2, g2, g2 + cube_size, g2 + cube_size, g2, g2, g2 + cube_size, g2 + cube_size],
-    z=[b2, b2, b2, b2, b2 + cube_size, b2 + cube_size, b2 + cube_size, b2 + cube_size],
-    i=[idx[0] for idx in faces_idx],
-    j=[idx[1] for idx in faces_idx],
-    k=[idx[2] for idx in faces_idx],
+    x=[0, r2, r2, 0, 0, r2, r2, 0],
+    y=[0, 0, g2, g2, 0, 0, g2, g2],
+    z=[0, 0, 0, 0, b2, b2, b2, b2],
     opacity=0.5,
     color='green',
     showscale=False
@@ -73,9 +57,9 @@ fig.add_trace(go.Mesh3d(
 # プロットの設定
 fig.update_layout(
     scene=dict(
-        xaxis=dict(title='R'),
-        yaxis=dict(title='G'),
-        zaxis=dict(title='B'),
+        xaxis=dict(title='R', range=[0, 255]),
+        yaxis=dict(title='G', range=[0, 255]),
+        zaxis=dict(title='B', range=[0, 255]),
     )
 )
 
