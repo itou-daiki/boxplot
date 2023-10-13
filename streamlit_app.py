1736b75657b53158a73eceedde07072f2705dfd3
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-13 05:00:14 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,24 @@ with col3:
 # 3Dプロットの作成
 fig = go.Figure()
 
+# 頂点のインデックスを定義（立方体を構成する各三角形の3つの頂点）
+faces_idx = [
+    [0, 1, 2], [0, 2, 3],  # bottom
+    [4, 5, 6], [4, 6, 7],  # top
+    [0, 1, 5], [0, 5, 4],  # front
+    [2, 3, 7], [2, 7, 6],  # back
+    [0, 3, 7], [0, 7, 4],  # left
+    [1, 2, 6], [1, 6, 5],  # right
+]
+
 # 立方体1
 fig.add_trace(go.Mesh3d(
     x=[0, r1, r1, 0, 0, r1, r1, 0],
     y=[0, 0, g1, g1, 0, 0, g1, g1],
     z=[0, 0, 0, 0, b1, b1, b1, b1],
+    i=[idx[0] for idx in faces_idx],
+    j=[idx[1] for idx in faces_idx],
+    k=[idx[2] for idx in faces_idx],
     opacity=0.5,
     color='red',
     showscale=False
@@ -46,6 +59,9 @@ fig.add_trace(go.Mesh3d(
     x=[0, r2, r2, 0, 0, r2, r2, 0],
     y=[0, 0, g2, g2, 0, 0, g2, g2],
     z=[0, 0, 0, 0, b2, b2, b2, b2],
+    i=[idx[0] for idx in faces_idx],
+    j=[idx[1] for idx in faces_idx],
+    k=[idx[2] for idx in faces_idx],
     opacity=0.5,
     color='green',
     showscale=False
